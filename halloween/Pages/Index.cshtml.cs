6a57bb4b7b3482cde3ed1e388459c42c5c6fdc91
meghannhumanfactors <31716705+meghannhumanfactors@users.noreply.github.com>
2017-10-08 17:03:49 -0700

    Add files via upload

---
+++ halloween/Pages/Index.cshtml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+
+namespace halloween.Pages
+{
+    public class IndexModel : PageModel
+    {
+        [BindProperty]
+        [Required(ErrorMessage = "Hey Dummy, that was rude!")]
+        public string mesgfromuser { get; set; }
+
+        [BindProperty]
+        public string recipient { get; set; }
+
+        [BindProperty]
+        public string subject { get; set; }
+
+        [BindProperty]
+        public string sendersname { get; set; }
+
+        [BindProperty]
+        public string sendersemail { get; set; }
+       
+
+        public void OnGet()
+        {
+
+        }
+        public void OnPost()
+        {
+
+        }
+
+
+    }     
+   
+}
